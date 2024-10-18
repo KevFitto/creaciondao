47617a163a21c19fdd1e8a35a08982da0284958e
KevFitto <kyfittoria@uamv.edu.ni>
2024-10-17 21:33:52 -0600

    tercer video correccion

--- src/main/java/org/kyfd/facturacion/modelo/Factura.java
+++ src/main/java/org/kyfd/facturacion/modelo/Factura.java
@@ -1,5 +1,54 @@
 package org.kyfd.facturacion.modelo;
 
+import java.time.*;
+import java.util.*;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+
+import org.hibernate.annotations.*;
+import org.kyfd.facturacion.calculadores.*;
+import org.openxava.annotations.*;
+import org.openxava.calculators.*;
+
+import lombok.*;
+
+@Entity
+@Getter @Setter
 public class Factura {
+
+	@Id
+	@GeneratedValue(generator = "System-uuid")
+	@Hidden
+	@GenericGenerator(name = "System-uuid", strategy = "uuid")
+	@Column(length = 32)
+	String oid;
+
+	@Column(length = 6)
+	@DefaultValueCalculator(value = CalculadorSiguienteNumeroParaAnyo.class,
+	properties = @PropertyValue(name = "anyo"))
+	int numero;
+
+	@DefaultValueCalculator(CurrentYearCalculator.class)
+	@Column(length = 4)
+	int anyo;
+
+	@DefaultValueCalculator(CurrentLocalDateCalculator.class)
+	@Required
+	LocalDate fecha;
+
+	@Stereotype("MEMO")
+	String observaciones;
+	@ManyToOne
+	(fetch=FetchType.LAZY,
+	optional=false)
+	Cliente cliente;
+
+	@ElementCollection
+	@ListProperties("producto.numero, producto.descripcion, cantidad")
+	Collection<Detalle> detalles;
+
+	@Stereotype("MEMO")
+	String observaciones1;
 
 }
